b526d8388c70dd3a5fb93235021c3e8ce9462f8e
jhessika-zarate <jhessikazarate@gmail.com>
2024-11-11 17:09:00 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <RouterView />
+</template>
+<script></script>
+
+<style >
+#app {
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  font-family: 'Avenir Next';
+  overflow-x: hidden;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <h1>hola</h1>
+  </div>
+  <div><h2>hola</h2></div>
+</template>
+
+<script>
+export default {
+  name: "ParallaxBannerWithCards",
+};
+</script>
+
+<style scoped>
+
+
+</style>
