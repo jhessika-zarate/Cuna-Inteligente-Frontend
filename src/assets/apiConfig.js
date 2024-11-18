5c77922ba4d69530b3c6c7e8a45191611351cc30
jhessika-zarate <jhessikazarate@gmail.com>
2024-11-17 20:33:59 -0400

    hsagxjhasxasx

--- src/assets/apiConfig.js
+++ src/assets/apiConfig.js
@@ -1,1 +1,3 @@
-export const RutaApi = import.meta.env.VITE_BACKEND_URL ||  "http://localhost:8080/api/v1/";
+export const RutaApi =
+  import.meta.env.VITE_BACKEND_URL ||
+  "https://backend-control-tareas-jhessika.serverbb.online/api/v1/";
