13baa9d69e5bc56fcf6af5bcbc6f4c100e733f8a
yomiisea <yomarrgm@gmail.com>
2024-11-17 23:21:45 -0400

    BotonesSeguimiento

--- src/views/Seguimiento.vue
+++ src/views/Seguimiento.vue
@@ -1,15 +1,26 @@
 <template>
+  
     <div class="main-container">
       <sidebar />
-      <div class="button-container">
-        <!-- Tarjetas con botones grandes -->
-        <div class="card" v-for="(option, index) in options" :key="index">
-          <button class="card-button">
-            <i :class="option.icon"></i>
-            <span>{{ option.label }}</span>
-          </button>
-        </div>
-      </div>
+      <div class="baby-info">
+
+<div class="info-grid">
+  <div class="info-card" style="background-image: url(/src/assets/5.png);">
+    <font-awesome-icon :icon="['fas', 'weight-scale']"
+      style="height: 5rem;color: whitesmoke;margin: 0.5rem;" />
+    <p>Peso:</p>
+  </div>
+  <div class="info-card" style="background-image: url(/src/assets/5.png);">
+    <font-awesome-icon :icon="['fas', 'ruler']" style="height: 5rem;color: whitesmoke;margin: 0.5rem;" />
+    <p>Peso:</p>
+  </div>
+  <div class="info-card" style="background-image: url(/src/assets/5.png);">
+    <font-awesome-icon :icon="['fas', 'baby']" style="height: 5rem;color: whitesmoke;margin: 0.5rem;" />
+    <p>Peso:</p>
+  </div>
+  
+</div>
+</div>
     </div>
   </template>
   
@@ -57,6 +68,51 @@
   max-width: 800px;
 }
 
+.baby-info {
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+
+}
+
+.info-grid {
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  /* Ajusta el número de columnas automáticamente */
+  gap: 20px;
+  /* Espaciado entre tarjetas */
+  margin-top: 20px;
+}
+
+.info-card {
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+.info-card:hover{
+  transform: scale(1.05);
+}
+
+.info-card i {
+
+  margin-bottom: 10px;
+}
+
+.info-card p {
+  color: white;
+  font-size: medium;
+  font-weight: 600;
+  font-family: Montserrat
+}
 .card {
   display: flex;
   justify-content: center;
